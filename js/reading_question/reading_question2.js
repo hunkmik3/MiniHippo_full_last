c749ebb1afad4e8c158033b11ef0a46a42fe95c7
Hunk Mike <135243373+hunkmik3@users.noreply.github.com>
2025-11-08 20:23:29 +0700

    Add/Update 1 lesson set(s) for Part 2

---
+++ js/reading_question/reading_question2.js
@@ -0,0 +1,26 @@
+// ===============================================================================================================
+// ////////////// DANH SÁCH CÂU HỎI PART 2 & 3 ///////////////
+// ===============================================================================================================
+
+// sadasd - Topic: ádasdad
+const question2Content_1 = [
+    'ád',
+    'ádasdasdasd',
+    'dsasadsa',
+    'sadsadas',
+    'sadsad',
+];
+
+const questionSets = [
+    question2Content_1,
+];
+
+const questheader1 = {
+    question2Content_1: "ádasdad",
+};
+
+function getQuestHeaders(obj) {
+    return Object.values(obj);
+}
+
+const questheader = getQuestHeaders(questheader1);
