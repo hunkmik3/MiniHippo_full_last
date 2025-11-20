17afe63ac1c3ee4be62b8aae48e91581f9818b46
Hunk Mike <135243373+hunkmik3@users.noreply.github.com>
2025-11-21 02:09:25 +0700

    Add new lesson: sdfsdfsds (1 sets)

---
+++ js/reading_question/reading_question2_lesson_1763665762524.js
@@ -0,0 +1,52 @@
+// ===============================================================================================================
+// ////////////// DANH SÁCH CÂU HỎI PART 2 & 3 ///////////////
+// ===============================================================================================================
+
+// sdfsdfsds - Topic: sdfsdfsds
+const question2Content_1 = [
+    'sdfsdfsds',
+    'sdfsdfsds',
+    'sdfsdfsds',
+    'sdfsdfsds',
+    'sdfsdfsds',
+];
+
+const questionSets = [
+    question2Content_1,
+];
+
+window.questionSets = questionSets;
+
+const questheader1 = {
+    question2Content_1: "sdfsdfsds",
+};
+
+function getQuestHeaders(obj) {
+    return Object.values(obj);
+}
+
+const questheader = getQuestHeaders(questheader1);
+
+/* MINI_HIPPO_LESSON_DATA_START
+{
+  "version": 1,
+  "lessonType": "reading",
+  "part": "2",
+  "sets": [
+    {
+      "id": 1,
+      "title": "sdfsdfsds",
+      "data": {
+        "topic": "sdfsdfsds",
+        "sentences": [
+          "sdfsdfsds",
+          "sdfsdfsds",
+          "sdfsdfsds",
+          "sdfsdfsds",
+          "sdfsdfsds"
+        ]
+      }
+    }
+  ]
+}
+MINI_HIPPO_LESSON_DATA_END */
